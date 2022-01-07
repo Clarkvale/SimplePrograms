5aea34a6bf9386fd46d55f36f91c9cdbc7cc1c03
Benjamin Clark <60831788+Clarkvale@users.noreply.github.com>
2022-01-07 16:32:55 -0500

    Updated from py2 w/ specific parameters

--- psuedogenome_SNPs.py
+++ psuedogenome_SNPs.py
@@ -9,10 +9,10 @@ Argument = sys.argv[1:]
 
 filename = Argument[0] #Reference fasta
 snpfile = Argument[1] #SNP file
-strain = Argument[2] 
+strain = int(Argument[2])
 
 def insert_newlines(string, every=60):
-	return '\n'.join(string[i:i+every] for i in xrange(0, len(string), every))
+	return '\n'.join(string[i:i+every] for i in range(0, len(string), every))
 
 
 def substitute_snp(header,genome,SNP):
@@ -27,53 +27,53 @@ def substitute_snp(header,genome,SNP):
 		#print SNP[snp][0]
 
 		if (genomearray[int(snp)-1]).lower() == (SNP[snp][0]).lower():
-	        	genomearray[int(snp)-1] = SNP[snp][1]
-	return  header+"\n"+insert_newlines("".join(genomearray))+"\n"
+			genomearray[int(snp)-1] = SNP[snp][1]
+	return header+"\n"+insert_newlines("".join(genomearray))+"\n"
 
 
-def nosnp_format(header,genome):
+def nosnp_format(header, genome):
 	name = ""
-        name = header
-        genomearray = []
-        genomearray = list(genome.rstrip(" "))
-        return  header+"\n"+insert_newlines("".join(genomearray))+"\n"
+	name = header
+	genomearray = []
+	genomearray = list(genome.rstrip(" "))
+	return  header+"\n"+insert_newlines("".join(genomearray))+"\n"
 
 		
 SNP = {}
 
 def trim(list):
-	listn = []
-        listn = list
-        newlist = []
-        for a in listn:
-        	newlist.append((a.rstrip()).lstrip())
-        return newlist
+	#listn = []
+	#listn = list
+	newlist = []
+	for a in list:
+		newlist.append((a.rstrip()).lstrip())
+	return newlist
 
 
 for line in fileinput.input([snpfile]):
-        if line.startswith("##") or not line.strip():
-                continue
-        else:
-        	info1 = []
-                info1 = line.rstrip("\n").split("\t")
+	if line.startswith("#") or not line.strip():
+		continue
+	else:
+		info1 = []
+		info1 = line.rstrip("\n").split("\t")
 
 		info = []
 		info = trim(info1)
 		if info[strain] != "." and info[strain].startswith("1/1:1"):
 			if "," not in info[4]:
 				if info[0] not in SNP:
-                			SNP[info[0]] = {}
-                        		SNP[info[0]][info[1]] = []
-                        		SNP[info[0]][info[1]].append(info[3])
+					SNP[info[0]] = {}
+					SNP[info[0]][info[1]] = []
+					SNP[info[0]][info[1]].append(info[3])
 					SNP[info[0]][info[1]].append(info[4])
 				else:
 					SNP[info[0]][info[1]] = []
-                                	SNP[info[0]][info[1]].append(info[3])
-                                	SNP[info[0]][info[1]].append(info[4])
+					SNP[info[0]][info[1]].append(info[3])
+					SNP[info[0]][info[1]].append(info[4])
 
 	#print SNP
 
-mousefile = open("Mus_musculus_SNPs.fa","w") #output file 
+mousefile = open("CN57_CAST_assembly.fa", "w") #output file
 
 genomefile = open(filename, 'r').read() #reading whole file in one string
 genomeb = []
@@ -89,15 +89,15 @@ for chr in genomeb[1:]:
 	genome = chr.split("\n")
 
 	header = ">"+str(genome[0])
-	print genome[0]
+	print(genome[0])
 
 	if (genome[0].rstrip(" ")).lstrip("chr") in SNP:
 		fasta = "".join(genome[1:])
-		dbafasta = substitute_snp(header,fasta,SNP[(genome[0].rstrip(" ")).lstrip("chr")])
+		dbafasta = substitute_snp(header, fasta, SNP[(genome[0].rstrip(" ")).lstrip("chr")])
 		mousefile.write(str(dbafasta))
 	else:
 		fasta = "".join(genome[1:])
-                dbafasta = nosnp_format(header,fasta)
-                mousefile.write(str(dbafasta))
+		dbafasta = nosnp_format(header, fasta)
+		mousefile.write(str(dbafasta))
 
 mousefile.close()
